7bec73dd1363e21bacac7216e2be73ab59dc68f2
Malikplays <73621163+Malikplays@users.noreply.github.com>
2025-08-25 21:53:14 -0500

    Update scraper.rb

--- scraper.rb
+++ scraper.rb
@@ -1,5 +1,13 @@
-require 'scraperwiki'
-require 'nokogiri'
+require 'sqlite3'
 
-ScraperWiki.save_sqlite(['name'], { name: 'ping', t: Time.now.to_i })
+# Create data.sqlite and a simple `data` table (morph.io shows this first)
+db = SQLite3::Database.new('data.sqlite')
+db.execute <<-SQL
+  CREATE TABLE IF NOT EXISTS data (
+    name TEXT PRIMARY KEY,
+    t INTEGER
+  );
+SQL
+db.execute("INSERT OR REPLACE INTO data (name, t) VALUES (?, ?)", ['ping', Time.now.to_i])
+
 puts "ok: wrote row to data.sqlite"
